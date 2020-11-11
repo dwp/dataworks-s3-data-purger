919dde5bfcb88c81aa82337382219ca2581190e4
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2020-11-11 11:15:11 +0000

    DW-5224-Delete-s3-files

--- s3_data_purger.py
+++ s3_data_purger.py
@@ -2,7 +2,7 @@
 import ast
 import json
 import logging
-
+import argparse
 import os
 from boto3.dynamodb.conditions import Key, Attr
 import boto3
@@ -15,7 +15,7 @@ DATA_PRODUCT_NAME = "data_product"
 
 def configure_log():
     """Configure JSON logger."""
-    log_level = os.environ.get("S3_DATA_PURGER_LOG_LEVEL", "INFO").upper()
+    log_level = args.log_level
     numeric_level = getattr(logging, log_level, None)
     if not isinstance(numeric_level, int):
         raise ValueError("Invalid log level: %s" % log_level)
@@ -38,11 +38,7 @@ def get_client(service_name):
     return client
 
 def get_resource(service_name):
-    #return boto3.resource(service_name, region_name="${aws_default_region}")
-    return boto3.resource(service_name, region_name="eu-west-2")
-
-def read_env_param(param_name):
-    return os.getenv(param_name)
+    return boto3.resource(service_name, region_name=args.aws_region)
 
 def get_list_keys_for_prefix(s3_client, s3_publish_bucket, s3_prefix):
     keys = []
@@ -65,11 +61,44 @@ def scan_table(dynamodb_resource,data_pipeline_metdata_table,data_product_name):
     return list_dates_dict
 
 
+def get_parameters():
+    """Define and parse command line args."""
+    parser = argparse.ArgumentParser(
+        description="Send arguments for the S3 data purger."
+    )
+
+    # Parse command line inputs and set defaults
+    parser.add_argument("--aws-region", default="eu-west-2")
+    parser.add_argument("--s3_publish_bucket", default="NOT_SET")
+    parser.add_argument("--data_pipeline_metdata_table", default="NOT_SET")
+    parser.add_argument("--s3_data_purger_log_level", default="INFO")
+
+    _args = parser.parse_args()
+
+    # Override arguments with environment variables where set
+    if "S3_PUBLISH_BUCKET" in os.environ:
+        _args.s3_publish_bucket = os.environ.get("S3_PUBLISH_BUCKET")
+
+    if "DATA_PIPELINE_METADATA_TABLE" in os.environ:
+        _args.data_pipeline_metdata_table = os.environ.get("DATA_PIPELINE_METADATA_TABLE")
+
+    if "S3_DATA_PURGER_LOG_LEVEL" in os.environ:
+        _args.log_level = os.environ.get("S3_DATA_PURGER_LOG_LEVEL").upper()
+
+    if "AWS_REGION" in os.environ:
+        _args.aws_region = os.environ["AWS_REGION"]
+
+    return _args
+
+args = get_parameters()
+
+
 def handler(event: dict = {}, context: object = None) -> dict:
     """Deletes old S3 files based on s3_publish_bucket,s3_prefix ,data_pipeline_metdata_table, DataProduct and num_of_retention_days parameters."""
     logger = configure_log()
-    s3_publish_bucket = read_env_param("S3_PUBLISH_BUCKET")
-    data_pipeline_metdata_table = read_env_param("DATA_PIPELINE_METADATA_TABLE")
+
+    s3_publish_bucket = args.s3_publish_bucket
+    data_pipeline_metdata_table = args.data_pipeline_metdata_table
 
     if S3_PREFIX in event and NUM_OF_RETENTION_DAYS in event and  DATA_PRODUCT_NAME in event:
         s3_prefix = event[S3_PREFIX]
